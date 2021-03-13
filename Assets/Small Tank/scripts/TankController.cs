8a5042d0d559c73192d4aa5ba3b7f399dbb47923
kaisei <khishida55@gmail.com>
2021-03-13 14:09:18 +0900

    add joystick

--- Assets/Small Tank/scripts/TankController.cs
+++ Assets/Small Tank/scripts/TankController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class TankController : MonoBehaviour
 {
@@ -9,18 +10,30 @@ public class TankController : MonoBehaviour
     public float forwardSpeed = 7.0f;
     public float backSpeed = 2.0f;
     public float rotateSpeed = 2.0f;
+
+    public GameObject master;
+    private VariableJoystick joystick;
+
+    private float v;
+    private float h;
+
+    public Vector3 centerOfMass; 
+    private Rigidbody rb;
     
     // Start is called before the first frame update
     void Start()
     {
+        joystick = master.GetComponent<GameMaster>().joystick.GetComponent<VariableJoystick>();
         
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = this.centerOfMass;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        float v = Input.GetAxis("Vertical"); //前なら+後ろなら-で取得(-1~+1)
-        float h = Input.GetAxis("Horizontal"); //右なら+左なら-で取得
+        this.v = readVertical(); //前なら+後ろなら-で取得(-1~+1)
+        this.h = readHorizontal(); //右なら+左なら-で取得
 
         velocity = new Vector3(0, 0, v); //移動量ベクトル
         if(v > 0.1) {
@@ -37,6 +50,32 @@ public class TankController : MonoBehaviour
 
         // Debug.Log("絶対回転量" + transform.rotation);
         
+    }
+
+    private float readVertical() {
+        float v = Input.GetAxis("Vertical");
+        if(Math.Abs(joystick.Vertical) >= Math.Abs(v)) { //もしjoystickの入力の方が大きいのなら
+            return joystick.Vertical;
+        } else {
+            return v;
+        }
+    }
+
+    private float readHorizontal() {
+        float h = Input.GetAxis("Horizontal");
+        if(Math.Abs(joystick.Horizontal) >= Math.Abs(h)) { //もしjoystickの入力の方が大きいのなら
+            return joystick.Horizontal;
+        } else {
+            return h;
+        }
+    }
+
+    public void setVertical(float v) {
+        this.v = v;
+    }
+
+    public void setHorizontal(float h) {
+        this.h = h;
     }
 
 }
--- Assets/scripts/GameMaster.cs
+++ Assets/scripts/GameMaster.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,10 +8,12 @@ public class GameMaster : MonoBehaviour
     public GameObject smallTank;
     public GameObject slider;
 
+    public GameObject joystick;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
